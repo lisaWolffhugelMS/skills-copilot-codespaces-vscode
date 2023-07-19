94a3826cd08dfe1c94675ed427043462f6317442
lisaWolffhugelMS <89141325+lisaWolffhugelMS@users.noreply.github.com>
2023-07-19 12:31:25 +0000

    copilot second commit

---
+++ member.js
@@ -0,0 +1,18 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var projects = document.getElementById("projects");
+    var contact = document.getElementById("contact");
+    var memberBox = document.getElementById("memberBox");
+    var skillsBox = document.getElementById("skillsBox");
+    var projectsBox = document.getElementById("projectsBox");
+    var contactBox = document.getElementById("contactBox");
+    member.style.backgroundColor = "#f2f2f2";
+    skills.style.backgroundColor = "#fff";
+    projects.style.backgroundColor = "#fff";
+    contact.style.backgroundColor = "#fff";
+    memberBox.style.display = "block";
+    skillsBox.style.display = "none";
+    projectsBox.style.display = "none";
+    contactBox.style.display = "none";
+}
